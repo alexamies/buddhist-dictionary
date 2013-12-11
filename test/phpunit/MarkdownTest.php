da3cf1116e739fde13ca08bb3404450df40e776c
alexamies <alex@chinesenotes.com>
2013-12-11 08:04:11 -0800

    Markdown enhancement

---
+++ test/phpunit/MarkdownTest.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Unit test for Markdown class.
+ */
+require_once dirname(__FILE__) . '/../../web/inc/markdown.php';
+
+class MarkdownTest extends PHPUnit_Framework_TestCase {
+
+    public function testTrivial() {
+        $text = 'Diamond Sutra Chapter 1 Sanskrit Text';
+        $markdown = new Markdown($text);
+        $html = $markdown->getHTML();
+        $this->assertTrue(strlen($html) > 0);
+        $this->assertTrue(strpos($html, $text) > -1);
+    }
+
+    public function testParagraphs1() {
+        $text = "Hello\n\n";
+        $markdown = new Markdown($text);
+        $html = $markdown->getHTML();
+        $this->assertTrue(strpos($html, '<p>Hello</p>') > -1);
+    }
+
+    public function testParagraphs2() {
+        $text = "Good bye\n \n";
+        $markdown = new Markdown($text);
+        $html = $markdown->getHTML();
+        $this->assertTrue(strpos($html, '<p>Good bye</p>') > -1);
+    }
+
+    public function testLinks() {
+        $text = "[an example](http://example.com/ 'Title')";
+        $markdown = new Markdown($text);
+        $html = $markdown->getHTML();
+        $expected = "<a href='http://example.com/' title='Title'>an example</a>";
+        $this->assertEqual(strpos($expected, $html);
+    }
+
+}
--- web/inc/markdown.php
+++ web/inc/markdown.php
@@ -11,7 +11,7 @@ class Markdown {
    *
    * @param $text   The plain text
    */
-  function Markdown($text) {
+  public function Markdown($text) {
     $this->text = $text;
   }
 
@@ -19,8 +19,37 @@ class Markdown {
    * Accessor method for the HTML
    * @return A string value
    */
-  function getHTML() {
-    return $this->text;
+  public function getHTML() {
+    
+    $html = preg_replace('{^\xEF\xBB\xBF|\x1A}', '', $this->text);  // Replace BOM
+    $html = preg_replace('{\r\n?}', "\n", $html);  // Mac and Windows to Unix linebreaks
+
+    $html = $this->replaceHeaders($html);
+    $html = $this->formParagraphs($html);
+
+    return $html;
   }
+
+  /**
+   * Form paragraphs from blank lines
+   *
+   * @return A string value
+   */
+  protected function formParagraphs($html) {
+    $html = preg_replace('{^(.*)\n\s*\n}m', "<p>$1</p>", $html);
+    return $html;
+  }
+
+  /**
+   * H1 and H2 Headers -> H2 headers
+   *
+   * @return A string value
+   */
+  protected function replaceHeaders($text) {
+    $html = preg_replace('{^(\#{1,2})[ ]+(.+?)[ ]*\#*\n+}xm', "<h2>$2</h2>\n", $text);  
+    $html = preg_replace('{^(\#{3})[ ]+(.+?)[ ]*\#*\n+}xm', "<h3>$2</h3>\n", $html);
+    return $html;
+  }
+
 }
 ?>
