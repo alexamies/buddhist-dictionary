55384e0407d332a5c95e9c4162058a974da5ba3d
alexamies <alexamies@gmail.com>
2014-12-11 20:12:23 -0800

    R script

--- r/generate_images.R
+++ r/generate_images.R
@@ -56,3 +56,19 @@ dev.off()
 png(filename="web/images/t_distribution_df5.png", width = 400, height = 400)
 curve(dt(x, df=5), -3, 3, xlab="x", ylab="Probability Density", col="blue")
 dev.off()
+
+# Box plot of a word frequencies in three texts
+gsz <- c(7.27, 5.76, 6.02, 3.00, 4.84, 5.92, 5.66, 3.69, 4.79, 4.76, 3.88, 5.35, 5.61, 5.03)
+luoyang <- c(6.66, 6.36, 7.45, 6.43, 4.99)
+mhzg <- c(5.89, 6.12, 4.27, 5.15, 4.22, 3.16, 4.28, 2.94, 3.35, 5.60)
+texts <- c("gsz", "gsz", "gsz", "gsz", "gsz", "gsz", "gsz", "gsz", "gsz", "gsz", "gsz", "gsz", 
+	       "gsz", "gsz", "luoyang", "luoyang", "luoyang", "luoyang", "luoyang", "mhzg", "mhzg", 
+	       "mhzg", "mhzg", "mhzg", "mhzg", "mhzg", "mhzg", "mhzg", "mhzg")
+png(filename="web/images/stats_word_freq_3texts.png", width = 400, height = 400)
+boxplot(frequencies ~ texts, xlab="Text", ylab="Frequency / Thousand Words", col="slategrey")
+dev.off()
+
+# Example histogram of random standard normal variables
+png(filename="web/images/stats_random_normal.png", width = 400, height = 400)
+hist(rnorm(10000), col="slategrey", xlab="Random values from a standard normal distribution")
+dev.off()
