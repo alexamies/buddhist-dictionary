9b47beb69e088a5786f8d8e935457ca2bcb797ed
alexamies <alexamies@gmail.com>
2014-12-13 18:25:44 -0800

    Added analytics to web pages.

--- web/phrase_detail.php
+++ web/phrase_detail.php
@@ -16,6 +16,14 @@
     <![endif]-->
   </head>
   <body>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+      ga('create', 'UA-57634593-1', 'auto');
+      ga('send', 'pageview');
+    </script>
     <div class="starter-template">
       <div class="row">
         <div class="span2"><img id="logo" src="images/yan.png" alt="Logo" class="pull-left"/></div>
@@ -82,6 +90,7 @@
         Copyright Nan Tien Institute 2013 - 2014, 
         <a href="http://www.nantien.edu.au/" title="Nan Tien Institute">www.nantien.edu.au</a>.
       </p>
+      <p>This page was last updated on December 13, 2014.</p>
     <div>
     <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
     <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
