1cddcc76c2160acf8dc91972ff98bfd4489feb61
alexamies <alexamies@gmail.com>
2016-07-16 21:08:46 -0700

    Change to deployment script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
-## Push changes from a staging environment to production
-if -z $PROD
-then
-  echo "PROD not set"
-  exit 1
-else
+## Push changes from staging environment $CNREADER_HOME to production $PROD
+if [ -n "$PROD" ]; then
   echo "Copying to $PROD"
+  if [ -n "$CNREADER_HOME" ]; then
+  	echo "Copying from $CNREADER_HOME"
+  	cp $CNREADER_HOME/web/words/*.html $PROD/web/words/.
+  	cp $CNREADER_HOME/web/analysis/*.html $PROD/web/analysis/.
+  	cp $CNREADER_HOME/web/analysis/taisho/*.html $PROD/web/analysis/taisho/.
+  	cp $CNREADER_HOME/web/taisho/*.html $PROD/web/taisho/.
+  	cp $CNREADER_HOME/data/dictionary/*.txt $PROD/data/dictionary/.
+  else
+    echo "CNREADER_HOME is not set"
+    exit 1
+  fi
+else
+  echo "PROD is not set"
+  exit 1
 fi
