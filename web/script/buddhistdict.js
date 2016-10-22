0b2c242ab73e596b4f3fa4f45048ffccd68b4b78
alexamies <alexamies@gmail.com>
2016-10-22 20:11:10 +0800

    Enable submission of text search via URL

--- web/script/buddhistdict.js
+++ web/script/buddhistdict.js
@@ -1,49 +1,47 @@
 // Angular code for main search page
 var textApp = angular.module('textApp', ['ngSanitize']);
 
-console.log("buddhistdict.js enter")
-
-textApp.controller('textCtrl', function($scope, $http, $sce, $location) {
-
-  var l = $location.search()
-  if (l.text) {
-    console.log("buddhistdict.js text = " + l.text)
-    $scope.formData.text = l.text
-    submitForm()
-  }
+textApp.controller('textCtrl', function($scope, $http, $location) {
+  var re = /[^\u0040-\u007F\u0080-\u00FF\u0100-\u017F\u0180-\u024F\u0300-\u036F]/;
   $scope.formData = {};
   $scope.formData.langtype = 'literary';
   $scope.formData.matchtype = 'approximate';
   $scope.results = {};
-  $scope.submit = submitForm()
-});
-
-function submitForm() {
-  var re = /[^\u0040-\u007F\u0080-\u00FF\u0100-\u017F\u0180-\u024F\u0300-\u036F]/;
-  $scope.results = {"msg": "Searching"};
-  var hasCJK = re.exec($scope.formData.text);  
-  var url = "/englishsearch.php";
-  if (hasCJK && ($scope.formData.matchtype == 'approximate')) {
-    url = "/textlookup.php";
-  }
-  $http({url: url, 
-         method: 'post', 
-         data: $.param($scope.formData),
-         headers : {'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8'}
-  }).success(function(data) {
-    $("#lookup-help-block").hide();
-    $("#word-detail").hide();
-    $scope.results = data;
-    //alert($scope.results.words)
-    if ($scope.results.words && $scope.results.words.length == 0) {
-      $scope.results = {"msg": "No results found"};
-    } else if ($scope.results.words && $scope.results.words.length == 1 
-               && $scope.results.words[0].headword) {
-      window.location = "/words/" + $scope.results.words[0].headword + ".html";
+  $scope.submit = function() {
+    $scope.results = {"msg": "Searching"};
+    var hasCJK = re.exec($scope.formData.text);  
+    var url = "/englishsearch.php";
+    if (hasCJK && ($scope.formData.matchtype == 'approximate')) {
+      url = "/textlookup.php";
     }
-  }).error(function(data, status, headers, config) {
-    $("#lookup-help-block").hide();
-    $scope.results = {"msg": data};
-    $("#word-detail").hide();
-  });
-}
+    $http({url: url, 
+           method: 'post', 
+           data: $.param($scope.formData),
+           headers : {'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8'}
+    }).success(function(data) {
+      $("#lookup-help-block").hide();
+      $("#word-detail").hide();
+      $scope.results = data;
+      //alert($scope.results.words)
+      if ($scope.results.words && $scope.results.words.length == 0) {
+        $scope.results = {"msg": "No results found"};
+      } else if ($scope.results.words && $scope.results.words.length == 1 
+                 && $scope.results.words[0].headword) {
+          window.location = "/words/" + $scope.results.words[0].headword + ".html";
+      }
+    }).error(function(data, status, headers, config) {
+      $("#lookup-help-block").hide();
+      $scope.results = {"msg": data};
+      $("#word-detail").hide();
+    });
+  };
+
+  // If the URL contains text then submit the form right away
+  var l = $location.search()
+  console.log("buddhistdict.js l = " + l)
+  if (l.text) {
+    console.log("buddhistdict.js text = " + l.text)
+    $scope.formData.text = l.text
+    $scope.submit()
+  }
+});
