d0f25143e653d4e10d3afdaff11ea5fcdc686d6c
alexamies <alexamies@gmail.com>
2016-12-17 10:26:48 -0800

    Words

--- bin/push.sh
+++ bin/push.sh
@@ -3,11 +3,11 @@
 ## PROD_SERVER should be set to the server name of the production system
 if [ -n "$PROD_SERVER" ]; then
   echo "Copying to $PROD_SERVER"
-  tar -czf tmp/taisho.tar.gz web/taisho
-  tar -czf tmp/words.tar.gz web/words
+  tar -czf tmp/taisho.tar.gz web/taisho web/analysis/taisho
+  tar -czf tmp/words.tar.gz web/words web/analysis
   if [ -n "$PROJECT" -a -n "$ZONE" ]; then
-      gcloud compute --project "$PROJECT" copy-files tmp/taisho.tar.gz $PROD_SERVER: --zone "$ZONE" 
-      gcloud compute --project "$PROJECT" copy-files tmp/words.tar.gz $PROD_SERVER: --zone "$ZONE" 
+      gcloud compute --project "$PROJECT" copy-files tmp/taisho.tar.gz $PROD_SERVER:/disk1/ntireadertest.org/tmp/ --zone "$ZONE" 
+      gcloud compute --project "$PROJECT" copy-files tmp/words.tar.gz $PROD_SERVER:/disk1/ntireadertest.org/tmp/ --zone "$ZONE" 
   else
     echo "Either PROJECT or ZONE is not set"
     exit 1
