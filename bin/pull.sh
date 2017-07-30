00da151bd7e0fef9cf7b0f01fe0ddbe3354f1807
alexamies <alexamies@gmail.com>
2017-07-29 17:44:22 -0700

    Deployment script

--- bin/pull.sh
+++ bin/pull.sh
@@ -2,11 +2,14 @@
 ## Pull changes from a GCS bucket
 ## BUCKET should be set to the name of the GCS bucket to store the generated
 if [ -n "$BUCKET" ]; then
-  echo "Copying to GCS bucket $BUCKET"
+  echo "Copying from GCS bucket $BUCKET"
+  gsutil cp gs://$BUCKET/taisho.tar.gz tmp/.
   tar -xzf tmp/taisho.tar.gz web/taisho web/analysis/taisho
   mv tmp/web/taisho/*.html web/taisho/.
+  gsutil cp gs://$BUCKET/words.tar.gz tmp/.
   tar -xzf tmp/words.tar.gz web/words web/analysis
   mv tmp/web/words/*.html web/words/.
+  mv tmp/web/analysis/*.html web/analysis/.
 else
   echo "Failed: BUCKET is not set, please set it to the name of the GCS bucket"
   exit 1
