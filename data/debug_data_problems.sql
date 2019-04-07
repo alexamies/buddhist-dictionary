0d9b172f89c0eec2e8af9ae3c210be30ab29c043
alexamies <alexamies@gmail.com>
2019-04-06 17:32:09 -0700

    Referential integrity fixes

--- data/debug_data_problems.sql
+++ data/debug_data_problems.sql
@@ -17,6 +17,26 @@ FROM
 WHERE
   id = 107129;
 
+/* Disable referential integrity*/
+SET @@foreign_key_checks = 0;
+
+/* Enable referential integrity*/
+SET @@foreign_key_checks = 1;
+
 /* Load rejected words. */
 LOAD DATA LOCAL INFILE 'rejected.tsv' INTO TABLE words CHARACTER SET utf8mb4 LINES TERMINATED BY '\n' IGNORE 1 LINES;
 show warnings;
+
+/* Find words with topics that do not match*/
+SELECT
+  id, topic_cn
+FROM
+  words
+WHERE
+  id NOT in (
+  SELECT
+    simplified
+  FROM
+    topics
+  )
+LIMIT 20;
