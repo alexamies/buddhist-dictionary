bc2fdb056023a3904339812f1e9f94294e7ed847
alexamies <alexamies@gmail.com>
2019-11-30 20:03:04 -0800

    JavaScript cleanup

--- data/load_data.sql
+++ data/load_data.sql
@@ -6,6 +6,7 @@ LOAD DATA LOCAL INFILE 'dictionary/words.txt' INTO TABLE words CHARACTER SET utf
 LOAD DATA LOCAL INFILE 'dictionary/fgs_mwe.txt' INTO TABLE words CHARACTER SET utf8mb4 LINES TERMINATED BY '\n';
 LOAD DATA LOCAL INFILE 'dictionary/translation_memory_buddhist.txt' INTO TABLE words CHARACTER SET utf8mb4 LINES TERMINATED BY '\n';
 LOAD DATA LOCAL INFILE 'dictionary/translation_memory_literary.txt' INTO TABLE words CHARACTER SET utf8mb4 LINES TERMINATED BY '\n';
+LOAD DATA LOCAL INFILE 'dictionary/translation_memory_modern.txt' INTO TABLE words CHARACTER SET utf8mb4 LINES TERMINATED BY '\n';
 LOAD DATA LOCAL INFILE 'dictionary/licenses.txt' INTO TABLE licenses CHARACTER SET utf8mb4 LINES TERMINATED BY '\r\n';
 LOAD DATA LOCAL INFILE 'dictionary/authors.txt' INTO TABLE authors CHARACTER SET utf8mb4 LINES TERMINATED BY '\r\n';
 LOAD DATA LOCAL INFILE 'dictionary/illustrations.txt' INTO TABLE illustrations CHARACTER SET utf8mb4 LINES TERMINATED BY '\r\n';
