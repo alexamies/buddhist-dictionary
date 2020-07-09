70d07354f3b11e56395eed3e14b39fa691dc37fb
jifusfo <jifusfo@outlook.com>
2020-07-08 21:11:08 -0700

    Words

--- data/load_data.sql
+++ data/load_data.sql
@@ -4,6 +4,7 @@ LOAD DATA LOCAL INFILE 'dictionary/grammar.txt' INTO TABLE grammar CHARACTER SET
 LOAD DATA LOCAL INFILE 'dictionary/topics.txt' INTO TABLE topics CHARACTER SET utf8mb4 LINES TERMINATED BY '\r\n';
 LOAD DATA LOCAL INFILE 'dictionary/words.txt' INTO TABLE words CHARACTER SET utf8mb4 LINES TERMINATED BY '\n' IGNORE 1 LINES;
 LOAD DATA LOCAL INFILE 'dictionary/fgs_mwe.txt' INTO TABLE words CHARACTER SET utf8mb4 LINES TERMINATED BY '\n';
+LOAD DATA LOCAL INFILE 'dictionary/buddhist_named_entities.txt' INTO TABLE words CHARACTER SET utf8mb4 LINES TERMINATED BY '\n';
 LOAD DATA LOCAL INFILE 'dictionary/translation_memory_buddhist.txt' INTO TABLE words CHARACTER SET utf8mb4 LINES TERMINATED BY '\n';
 LOAD DATA LOCAL INFILE 'dictionary/translation_memory_literary.txt' INTO TABLE words CHARACTER SET utf8mb4 LINES TERMINATED BY '\n';
 LOAD DATA LOCAL INFILE 'dictionary/translation_memory_modern.txt' INTO TABLE words CHARACTER SET utf8mb4 LINES TERMINATED BY '\n';
