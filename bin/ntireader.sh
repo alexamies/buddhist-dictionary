f4732826e293a26543a77873a850788f621958cd
jifusfo <jifusfo@outlook.com>
2020-11-21 10:03:57 -0800

    Template fix

--- bin/ntireader.sh
+++ bin/ntireader.sh
@@ -27,9 +27,9 @@ go run github.com/alexamies/cnreader -tmindex
 mkdir $WEB_DIR/dist
 cp web-resources/dist/*.css $WEB_DIR/dist/.
 cp web-resources/dist/*.js $WEB_DIR/dist/.
-cp web-resources/*.css $WEB_DIR/.
 cp web-resources/script/*.js $WEB_DIR/script/.
 cp web-resources/*.js $WEB_DIR/.
 cp web-resources/images/*.* $WEB_DIR/images/.
 
 python3 bin/words2json.py "data/dictionary/words.txt,data/dictionary/fgs_mwe.txt,data/dictionary/translation_memory_buddhist.txt,data/dictionary/translation_memory_literary.txt,data/dictionary/buddhist_named_entities.txt" $WEB_DIR/dist/ntireader.json
+echo 'ntireader done'
