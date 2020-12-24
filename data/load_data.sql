66336fd70b712143252f801d361bc859e6bdd205
jifusfo <jifusfo@outlook.com>
2020-12-23 22:46:11 -0800

    Words

--- data/load_data.sql
+++ data/load_data.sql
@@ -9,6 +9,7 @@ LOAD DATA LOCAL INFILE 'data/dictionary/fgs_mwe.txt' INTO TABLE words CHARACTER 
 LOAD DATA LOCAL INFILE 'data/dictionary/buddhist_named_entities.txt' INTO TABLE words CHARACTER SET utf8mb4 LINES TERMINATED BY '\n' IGNORE 1 LINES;;
 LOAD DATA LOCAL INFILE 'data/dictionary/translation_memory_buddhist.txt' INTO TABLE words CHARACTER SET utf8mb4 LINES TERMINATED BY '\n' IGNORE 1 LINES;;
 LOAD DATA LOCAL INFILE 'data/dictionary/translation_memory_literary.txt' INTO TABLE words CHARACTER SET utf8mb4 LINES TERMINATED BY '\n' IGNORE 1 LINES;;
+LOAD DATA LOCAL INFILE 'data/dictionary/translation_memory_hsingyun.txt' INTO TABLE words CHARACTER SET utf8mb4 LINES TERMINATED BY '\n' IGNORE 1 LINES;
 LOAD DATA LOCAL INFILE 'data/dictionary/licenses.txt' INTO TABLE licenses CHARACTER SET utf8mb4 LINES TERMINATED BY '\r\n' IGNORE 1 LINES;;
 LOAD DATA LOCAL INFILE 'data/dictionary/authors.txt' INTO TABLE authors CHARACTER SET utf8mb4 LINES TERMINATED BY '\r\n' IGNORE 1 LINES;;
 LOAD DATA LOCAL INFILE 'data/dictionary/illustrations.txt' INTO TABLE illustrations CHARACTER SET utf8mb4 LINES TERMINATED BY '\r\n' IGNORE 1 LINES;;
