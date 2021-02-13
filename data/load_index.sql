f4de75a253926c83558fc5f792d58d4234df533b
jifusfo <jifusfo@outlook.com>
2021-02-12 22:13:55 -0800

    Schema update

--- data/load_index.sql
+++ data/load_index.sql
@@ -1,7 +1,5 @@
 USE ntireader;
 
 delete from collection;
-delete from document;
 
 LOAD DATA LOCAL INFILE 'data/corpus/collections.csv' INTO TABLE collection CHARACTER SET utf8mb4 LINES TERMINATED BY '\n' IGNORE 2 LINES;
-LOAD DATA LOCAL INFILE 'data/corpus/documents.csv' INTO TABLE document CHARACTER SET utf8mb4 LINES TERMINATED BY '\n';
--- index/load_word_freq.sql
+++ index/load_word_freq.sql
@@ -1,8 +1,12 @@
 use ntireader;
 
+DELETE FROM document;
 DELETE FROM tmindex_uni_domain;
 DELETE FROM tmindex_unigram;
+DELETE FROM word_freq_doc;
+DELETE FROM bigram_freq_doc;
 
+LOAD DATA LOCAL INFILE 'index/documents.tsv' INTO TABLE document CHARACTER SET utf8mb4 LINES TERMINATED BY '\n';
 LOAD DATA LOCAL INFILE 'index/tmindex_uni_domain.tsv' INTO TABLE tmindex_uni_domain CHARACTER SET utf8mb4 LINES TERMINATED BY '\n';
 LOAD DATA LOCAL INFILE 'index/tmindex_unigram.tsv' INTO TABLE tmindex_unigram CHARACTER SET utf8mb4 LINES TERMINATED BY '\n';
 LOAD DATA LOCAL INFILE 'index/word_freq_doc.txt' INTO TABLE word_freq_doc CHARACTER SET utf8mb4 LINES TERMINATED BY '\n';
