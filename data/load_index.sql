25d1df983842668641f2805601d6008d57889da6
alexamies <alexamies@gmail.com>
2022-11-09 19:43:53 -0800

    Buddhist terminology

--- data/load_index.sql
+++
@@ -1,5 +0,0 @@
-USE ntireader;
-
-delete from collection;
-
-LOAD DATA LOCAL INFILE 'data/corpus/collections.csv' INTO TABLE collection CHARACTER SET utf8mb4 LINES TERMINATED BY '\n' IGNORE 2 LINES;
